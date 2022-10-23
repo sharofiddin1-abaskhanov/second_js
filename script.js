746d1664b6fdc0d6f3c3a654ac0acc8f554a05c5
Sharofiddin <sdfdfdf@dfdfd.gfg>
2022-10-23 20:56:43 +0500

    HTML

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+var userName = prompt('Ismingizni kiriting')
+
+var userAge = +prompt('Yoshingizni kiriting')
+
+alert('Salom ' + userName + ',' + ' Quyidagi keltirilgan misollar ketma-ketligini yeching ')
+
+var qushuv = +prompt(' 1)    5 + 7 = ')
+var ayiruv = +prompt(' 2)   88 - 17 = ')
+var kupayturuv = +prompt('3)  12 * 8 = ')
+var bulish = +prompt('4)  91 / 7 = ')
+var qoldiqli = +prompt(' 5)   52 % 31 = ')
+
+
+
+console.info (' Sizning javobingiz : ' + ' 1) ' + qushuv, ' 2) ' +  ayiruv, ' 3) ' + kupayturuv, ' 4) ' + bulish, ' 5) ' + qoldiqli  +
+'  Misollarning javoblari : 1) 12 , 2) 71 , 3) 96 ,  4) 13 , 5) qoldiq : 1 ')
