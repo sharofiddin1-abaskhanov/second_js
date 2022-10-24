d150f32d246209f791d3845dc31318d311861fb3
Sharofiddin <sdfdfdf@dfdfd.gfg>
2022-10-24 07:31:47 +0500

    main JS

--- script.js
+++ script.js
@@ -13,7 +13,14 @@ var bulish = +prompt('4)  91 / 7 = ')
 var qoldiqli = +prompt(' 5)   52 % 31 = ')
 
 
+console.info (' Sizning ismingiz ' + userName)
+console.info (' Sizning yoshingiz '+ userAge)
+console.info (' Javob  1)  5 + 7 = 12  / Sizning javob '+ qushuv)
+console.info (' Javob  2)  88 - 17 = 71  / Sizning javob '+ ayiruv)
+console.info (' Javob  3)  12 * 8 = 96  / Sizning javob '+ kupayturuv)
+console.info (' Javob  4)  91 / 7 = 13  / Sizning javob '+ bulish)
+console.info (' Javob  5)  52 % 31 = qoldiq : 1  / Sizning javob '+ qoldiqli)
 
 
-console.info (' Sizning javobingiz : ' + ' 1) ' + qushuv, ' 2) ' +  ayiruv, ' 3) ' + kupayturuv, ' 4) ' + bulish, ' 5) ' + qoldiqli  +
-'  Misollarning javoblari : 1) 12 , 2) 71 , 3) 96 ,  4) 13 , 5) qoldiq : 1 ')
+// console.info (' Sizning javobingiz : ' + ' 1) ' + qushuv, ' 2) ' +  ayiruv, ' 3) ' + kupayturuv, ' 4) ' + bulish, ' 5) ' + qoldiqli  +
+// '  Misollarning javoblari : 1) 12 , 2) 71 , 3) 96 ,  4) 13 , 5) qoldiq : 1 ')
