c73541e8bdda7fbfba76865da42e2d909bbfaac1
Sharofiddin <sdfdfdf@dfdfd.gfg>
2022-10-30 00:19:37 +0500

    master

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 
+// Homework 1
 
-var userName = prompt('Ismingizni kiriting')
+var user1 = +prompt(' Yoshingizni kirirting! ')
 
-var userAge = +prompt('Yoshingizni kiriting')
+if( user1 > 0 && user1 <= 18 ) {
+    alert ( ' Siz hali yoshsiz, Oqishingiz kerak')
+}
 
-alert('Salom ' + userName + ',' + ' Quyidagi keltirilgan misollar ketma-ketligini yeching ')
+else if ( user1 > 18 && user1 <= 50) {
+    alert ( ' Siz ishlashingiz kerak!')
+}
 
-var qushuv = +prompt(' 1)    5 + 7 = ')
-var ayiruv = +prompt(' 2)   88 - 17 = ')
-var kupayturuv = +prompt('3)  12 * 8 = ')
-var bulish = +prompt('4)  91 / 7 = ')
-var qoldiqli = +prompt(' 5)   52 % 31 = ')
+else if ( user1 > 50 && user1 <= 59) {
+    alert ( ' Siz yaqinda nafaqaga ketasiz!')
+}
 
+else if ( user1 > 59 && user1 <= 150) {
+    alert ( ' Siz nafaqaxursiz!')
+}
 
-console.info (' Sizning ismingiz ' + userName)
-console.info (' Sizning yoshingiz '+ userAge)
-console.info (' Javob  1)  5 + 7 = 12  / Sizning javob '+ qushuv)
-console.info (' Javob  2)  88 - 17 = 71  / Sizning javob '+ ayiruv)
-console.info (' Javob  3)  12 * 8 = 96  / Sizning javob '+ kupayturuv)
-console.info (' Javob  4)  91 / 7 = 13  / Sizning javob '+ bulish)
-console.info (' Javob  5)  52 % 31 = qoldiq : 1  / Sizning javob '+ qoldiqli)
-
-
-// console.info (' Sizning javobingiz : ' + ' 1) ' + qushuv, ' 2) ' +  ayiruv, ' 3) ' + kupayturuv, ' 4) ' + bulish, ' 5) ' + qoldiqli  +
-// '  Misollarning javoblari : 1) 12 , 2) 71 , 3) 96 ,  4) 13 , 5) qoldiq : 1 ')
+else {
+    alert (' Nimadir notugri ketti ')
+}
